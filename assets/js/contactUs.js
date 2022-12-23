f21a766d8a34b291184e04317f419d8e827b1aed
proxod3 <63583253+proxod3-first@users.noreply.github.com>
2022-12-23 06:41:58 +0300

    modified:   assets/js/contactUs.js modified:   contactUs.html modified:   wallet.html

--- assets/js/contactUs.js
+++ assets/js/contactUs.js
@@ -1,11 +1,10 @@
-$(".form").on("click", function (e) {
-  var fname = document.getElementById("fname").value;
-  var lname = document.getElementById("lname").value;
-  var email = document.getElementById("email").value;
-  if (fname === "")
-    return false;
-  else if (lname === "")
-    return false;
-  else if (email === "") 
-    return false;
-});
+document.querySelector("form").addEventListener(
+  "click",
+  () =>
+    function (e) {
+      var fname = document.getElementById("fname").value;
+      var lname = document.getElementById("lname").value;
+      if (fname === "") return false;
+      else if (lname === "") return false;
+    }
+);
